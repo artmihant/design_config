d5ec0a97aefa43ad5f1fd8dd9d627a03f7c9046e
Artem Antonov <antonov@saldlab.com>
2023-01-17 18:45:39 +0300

    +

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='design_config',
-      version='0.0.2',
+      version='0.0.3',
       description='Smart tiny config library for flask-like config',
-      long_description='Smart tiny config library for flask-like config',
+      long_description='https://github.com/artmihant/artmihant',
       packages=['design_config'],
       author_email='artmihant@gmail.com',
       zip_safe=False)
